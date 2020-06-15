b71fca125f6e5a93f1c576eaa405e20cf20a8213
Volodymyr Trach <50409925+VolodymyrTrach@users.noreply.github.com>
2020-06-15 21:56:00 +0300

    Add files via upload

---
+++ server.js
@@ -0,0 +1,27 @@
+const express = require('express')
+const app = express()
+const expressGraphql = require('express-graphql')
+const {
+    GraphQLSchema,
+    GraphQLObjectType,
+    GraphQLString
+} = require('graphql')
+
+const mySchema = new GraphQLSchema({
+    query: new GraphQLObjectType({
+        name: 'Helloworld',
+        fields: () => ({
+            massage: {
+                type: GraphQLString,
+                resolve: () => 'Hello World !!!'
+            }
+        })
+    })
+})
+app.use('/graphql', expressGraphql({
+    schema: mySchema,
+    graphiql: true
+}))
+app.listen(5000, () => {
+    console.log('Server listening on port 5000!');
+});
